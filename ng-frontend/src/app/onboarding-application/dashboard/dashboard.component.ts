93f07826882cdf0b8d84a34da84fdd6548307e8c
Raotosamy <hraotosamy@gmail.com>
2024-12-09 20:20:43 +0300

    Fetch data from /applications

--- ng-frontend/src/app/onboarding-application/dashboard/dashboard.component.ts
+++ ng-frontend/src/app/onboarding-application/dashboard/dashboard.component.ts
@@ -1,45 +1,36 @@
-import { Component } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 
 
 @Component({
   selector: 'app-dashboard',
-  imports: [MatTableModule, MatMenuModule, MatButtonModule],
+  standalone: true,
+  imports: [
+    MatTableModule, 
+    MatMenuModule, 
+    MatButtonModule, 
+    HttpClientModule, 
+    MatPaginatorModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
-  public displayedColumns: string[] = ['id', 'company', 'entityId', 'businessId', 'country', 'regNumber', 'actions'];
+
+export class DashboardComponent implements AfterViewInit{
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  public displayedColumns: string[] = ['company', 'entityId', 'businessId', 'country', 'regNumber', 'actions'];
   
-  public dataSource = [
-    {
-      "id":"#uid",
-      "name":"Raotosamy",
-      "email":"contact@raoto.net",
-      "typeId":1,
-      "company":"RAOTO.NET",
-      "entityId":1,
-      "businessId":1,
-      "licence":"Not defined",
-      "regNumber":"0000-",
-      "country":"Madagascar",
-      "dateInc":"2000-12-25",
-      "passeport":"xxxx-",
-      "processStatus":0,
-      "approverStatus":0
-    }
-  ];
+  public dataSource = new MatTableDataSource<any>([]);
 
   private entityMap: { [key: number]: string } = {
     1:"Trust",
     2:"Association",
     3:"Private Company"
-  }
-
-  getEntityName(entityId: number){
-    return this.entityMap[entityId];
   }
 
   private businessMap: { [key: number]: string } = {
@@ -48,7 +39,32 @@ export class DashboardComponent {
     3:"Manufacturing"
   }
 
+  constructor(private http: HttpClient, private router: Router) {
+    this.fetchApplicationsList();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  getEntityName(entityId: number){
+    return this.entityMap[entityId];
+  }
+
   getBusinessName(businessId: number){
     return this.businessMap[businessId];
   }
+
+  fetchApplicationsList(){
+    this.http.get<any>('http://localhost:8081/applications?page=0&size=100').subscribe({
+      next: (response) => {
+        this.dataSource.data = response._embedded.applications;
+      },
+      error: (error) => {
+        console.error('Loading failed:', error);
+        this.dataSource.data = [];
+      }
+    });
+  }
+
 }
